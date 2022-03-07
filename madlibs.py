972636693558761838ba187cdd63dbdc400dd458
Shain-H <hodzic.shain@gmail.com>
2022-03-07 16:58:05 -0500

    Added user input Python

---
+++ madlibs.py
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>madlibs</title>
+</head>
+<body>
+    <script>
+        var noun = prompt("Insert a Noun!");
+        var adjective = prompt("Insert a Adjective!");
+        var adverb = prompt("Insert a Adverb!");
+        var verb = prompt("Insert a verb!");
+
+        console.log("On a tropical " + noun + ", Underneath a " + adjective + " lava moon. " + verb + " the hula dancers, Askin' questions cause' they " + adverb + " got all the answers.");
+
+    </script>
+</body>
+</html>
