5ca81f14ce1b4c10b846573e8741e7d82d17659c
Shain-H <hodzic.shain@gmail.com>
2022-03-07 17:00:50 -0500

    Fixed Madlibs Py version!

--- madlibs.py
+++ madlibs.py
@@ -1,20 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>madlibs</title>
-</head>
-<body>
-    <script>
-        var noun = prompt("Insert a Noun!");
-        var adjective = prompt("Insert a Adjective!");
-        var adverb = prompt("Insert a Adverb!");
-        var verb = prompt("Insert a verb!");
 
-        console.log("On a tropical " + noun + ", Underneath a " + adjective + " lava moon. " + verb + " the hula dancers, Askin' questions cause' they " + adverb + " got all the answers.");
+noun = input("Insert a Noun!")
+adjective = input("Insert a Adjective!")
+adverb = input("Insert a Adverb!")
+verb = input("Insert a verb!")
 
-    </script>
-</body>
-</html>
+print("On a tropical "+ noun +", Underneath a " + adjective + " lava moon. " + verb + " the hula dancers, Askin' questions cause' they " + adverb + " got all the answers.")
